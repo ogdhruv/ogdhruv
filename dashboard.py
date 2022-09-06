6e7dd89cfb1aef444eed4039a3095b6fe7647741
ogdhruv <dhruvrohilla.has@outlook.com>
2022-09-06 18:07:00 +0530

    changed Intro panel width

--- dashboard.py
+++ dashboard.py
@@ -9,8 +9,8 @@ tree = Tree("[link=https://www.github.com/ogdhruv]Dhruv",
             guide_style="bold cyan")
 my_tree = tree.add("Learning python", guide_style="bold purple")
 my_tree.add("Right now on the #100DayofWeb")
-my_tree.add("On the road to Django web development and DevOps")
-
+my_tree.add("Django and Javascript")
+my_tree.add("DevOps")
 project = tree.add("My projects", guide_style="bold green")
 project.add("will add soon")
 
@@ -18,13 +18,13 @@ project.add("will add soon")
 about = """
 Hi, i am Dhruv, a sophomore pursuing my bachelors in Computer Science and Engineering.
 I have a passion of learning everything.
-I'm learning Python right now, most of my time is
-spent on terminal and DSA.\
+I'm learning Python right now, most of my time
+is spent on terminal and DSA.
 """
 
 
 panel = Panel.fit(about, box=box.HEAVY, border_style="red",
-                  title="[bold]Hello fellow dev!", width=60)
+                  title="[bold]Hello fellow dev!", width=50)
 
 console.print(Columns([panel, tree]))
 
